7e3e451a1bd937c008eb51cfe7ec74c0e7f693bd
Michal Hromas <michalhromas1@gmail.com>
2021-02-06 22:36:29 +0100

    Deep equal

--- Chapter 04/4.4 - Deep Comparison/deep-comparison.js
+++ Chapter 04/4.4 - Deep Comparison/deep-comparison.js
@@ -1,3 +1,21 @@
-function minimum(a, b) {
-  return b > a ? a : b;
+function deepEqual(a, b) {
+  const areObjects = typeof a === 'object' && typeof b === 'object';
+  const areSomeNull = a === null || b === null;
+
+  if (!areObjects || areSomeNull) return a === b;
+  if (Object.keys(a).length !== Object.keys(b).length) return false;
+
+  let areEqualObjects = true;
+  for (const prop in a) {
+    if (!deepEqual(a[prop], b[prop])) {
+      areEqualObjects = false;
+      break;
+    }
+  }
+  return areEqualObjects;
 }
+
+const a = { prop: 'a', prop2: { a: 'b' } };
+const b = { prop: 'a', prop2: { a: 'b' } };
+
+console.log(deepEqual(a, b));
