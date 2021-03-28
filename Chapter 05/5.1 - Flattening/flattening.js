cb059944c3603d5e38a61e63ddb3145751287f23
Michal Hromas <michalhromas1@gmail.com>
2021-03-28 19:05:57 +0200

    Chapter 05

--- Chapter 05/5.1 - Flattening/flattening.js
+++ Chapter 05/5.1 - Flattening/flattening.js
@@ -2,4 +2,15 @@ function flatten(array) {
   return array.reduce((a, b) => a.concat(b), []);
 }
 
-console.log(flatten([1, 2, [1, 2, 3], [2, 3, 4], [3, 2, 5]]));
+function flattenRecursive(value, result = []) {
+  if (!value.forEach) {
+    result.push(value);
+  } else {
+    value.forEach((v) => flattenRecursive(v, result));
+  }
+
+  return result;
+}
+
+console.log(flatten([1, 2, [1, 2, 3], [2, [3], 4], [3, 2, 5]]));
+console.log(flattenRecursive([1]));
