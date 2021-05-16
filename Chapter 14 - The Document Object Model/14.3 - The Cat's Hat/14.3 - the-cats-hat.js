def07ad5328171b819f7be637ea437655fd5ac21
Michal Hromas <michalhromas1@gmail.com>
2021-05-16 17:12:42 +0200

    Chapter 14

---
+++ Chapter 14 - The Document Object Model/14.3 - The Cat's Hat/14.3 - the-cats-hat.js
@@ -0,0 +1,17 @@
+let cat = document.querySelector('#cat');
+let hat = document.querySelector('#hat');
+
+let angle = 0;
+let lastTime = null;
+
+function animate(time) {
+  if (lastTime != null) angle += (time - lastTime) * 0.001;
+  lastTime = time;
+  cat.style.top = Math.sin(angle) * 40 + 40 + 'px';
+  cat.style.left = Math.cos(angle) * 200 + 230 + 'px';
+
+  // Your extensions here.
+
+  requestAnimationFrame(animate);
+}
+requestAnimationFrame(animate);
