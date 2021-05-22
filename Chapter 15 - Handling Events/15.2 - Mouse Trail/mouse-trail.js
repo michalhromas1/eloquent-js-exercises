232b3d725c78f69cdac298a600e34b70f17420a2
Michal Hromas <michalhromas1@gmail.com>
2021-05-22 15:21:11 +0200

    Chapter 15

--- Chapter 15 - Handling Events/15.2 - Mouse Trail/mouse-trail.js
+++ Chapter 15 - Handling Events/15.2 - Mouse Trail/mouse-trail.js
@@ -1,21 +1,50 @@
-const trails = [];
 const TRAIL_COUNT = 20;
+const TRAIL_CLASS = 'trail';
+const DELAY = 10;
 
-for (let i = 0; i < TRAIL_COUNT; i++) {
-  const trail = document.createElement('div');
-  trail.className = 'trail';
-  document.body.appendChild(trail);
-  trails.push(trail);
+(function () {
+  let posX = 0;
+  let posY = 0;
+
+  const trails = createTrails(TRAIL_COUNT);
+  document.body.append(...trails);
+  document.addEventListener('mousemove', onMouseMove);
+
+  function onMouseMove(e) {
+    posX = e.pageX;
+    posY = e.pageY;
+
+    requestAnimationFrame(() => animate());
+  }
+
+  async function animate() {
+    for (let i = 0; i < TRAIL_COUNT; i++) {
+      moveTrail(trails[i]);
+
+      if (i + 1 !== TRAIL_COUNT) {
+        await delay(DELAY);
+      }
+    }
+  }
+
+  function moveTrail(trail) {
+    trail.style.left = `${posX}px`;
+    trail.style.top = `${posY}px`;
+  }
+})();
+
+function createTrails(count) {
+  const trails = [];
+
+  for (let i = 0; i < count; i++) {
+    const trail = document.createElement('div');
+    trail.classList.add(TRAIL_CLASS);
+    trails.push(trail);
+  }
+
+  return trails;
 }
 
-let currentTrail = 0;
-
-document.addEventListener('mousemove', (e) => {
-  const x = e.pageX;
-  const y = e.pageY;
-
-  trails[currentTrail].style.top = y + 'px';
-  trails[currentTrail].style.left = x + 'px';
-
-  currentTrail = (currentTrail + 1) % TRAIL_COUNT;
-});
+async function delay(time) {
+  await new Promise((resolve) => setTimeout(resolve, time));
+}
